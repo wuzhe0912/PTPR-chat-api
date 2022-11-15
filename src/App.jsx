5f9aa9c826ed86d5bf743f5b2700433157f35c4d
Pitt Wu <kgb00128@gmail.com>
2022-11-15 23:50:43 +0800

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 import 'normalize.css';
 
 function App() {
-  return <div className='App'>Cryptocurrency App</div>;
+  return (
+    <div className='app'>
+      <nav className='navbar'></nav>
+      <main className='main'></main>
+      <footer className='footer'></footer>
+    </div>
+  );
 }
 
 export default App;
